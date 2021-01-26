9749d18e7a31fa0912620c02964932918468c8b4
Justin Korn <justinkorn@Justins-MacBook-Pro.local>
2021-01-26 09:23:14 -0800

    Creating array/adding input

--- javacode.js
+++ javacode.js
@@ -4,10 +4,32 @@ function buttonClick () {
   var fname = document.getElementById('fname').value;
   var lname = document.getElementById('lname').value;
   console.log(fname, lname);
-  div.innerHTML = "\n";
-  div.innerHTML += "\t\t<h1>That's me!</h1>\n"
-  div.innerHTML += "\t\t<img src='picture.jpg' />\n"
-  div.innerHTML += "\t\t<p>" + fname + " " + lname + "</p>\n";
+
+  myJSON = {
+    'firstName': fname,
+    'lastName': lname,
+  }
+  strData2 = localStorage.getItem("fname/lname");
+  data = JSON.parse(strData2);
+  // Create array to store JSON objects
+  data.push(myJSON);
+  strData = JSON.stringify(data);
+  localStorage.setItem("fname/lname", strData);
+
+  console.log(localStorage);
+  console.log(myJSON);
+  console.log(data);
+
+  if (myJSON['firstName'] == 'justin' || 'Justin') {
+    div.innerHTML = "\n";
+    div.innerHTML += "\t\t<h1>That's me!</h1>\n"
+    div.innerHTML += "\t\t<img src='picture.jpg' />\n"
+    div.innerHTML += "\t\t<p>" + myJSON['firstName'] + " " + myJSON['lastName'] + "</p>\n";
+  } else {
+    div.innerHTML = "\n";
+    div.innerHTML += "\t\t<p>That's not me!<p>";
+  }
+
 }
 
 div2 = document.getElementById("div2");
@@ -15,16 +37,41 @@ div2 = document.getElementById("div2");
 function buttonClick2 () {
   var justin = document.getElementById('justin');
   var bryan = document.getElementById('bryan');
+  myJSON = {
+    'justin': 'Justin',
+    'bryan': 'Bryan',
+    'justinCheck': justin.checked,
+    'bryanCheck': bryan.checked,
+  }
 
-  if (justin.checked) {
+  if (myJSON['justinCheck']) {
     div2.innerHTML = "\n";
     div2.innerHTML += "\t\t<h1>That's also me!</h1>\n"
     div2.innerHTML += "\t\t<img src='picture.jpg' />\n"
-    div2.innerHTML += "\t\t<p>" + 'Justin' + " " + 'Korn' + "</p>\n";
-  } else if (bryan.checked) {
+    div2.innerHTML += "\t\t<p>" + myJSON['justin'] + " " + 'Korn' + "</p>\n";
+  } else if (myJSON['bryanCheck']) {
     div2.innerHTML = "\n";
     div2.innerHTML += "\t\t<h1>That's not me!</h1>\n"
     div2.innerHTML += "\t\t<img src='Bryan.png' />\n"
-    div2.innerHTML += "\t\t<p>" + 'Bryan' + " " + 'Korn' + "</p>\n";
+    div2.innerHTML += "\t\t<p>" + myJSON['bryan'] + " " + 'Korn' + "</p>\n";
+  } else {
+    div2.innerHTML = "\n";
+    div2.innerHTML += "\t\t<p>You didn't select a name!</p>\n";
   }
 }
+
+// Test code for local Storage.
+//div3 = document.getElementById("div3");
+//function userPass() {
+//  var uname = document.getElementById('uname').value;
+//  var pword = document.getElementById('pword').value;
+//  var unamedata = localStorage.getItem("uname")
+//  if (unamedata != "") {
+
+//    div3.innerHTML = "\n";
+//    document.getElementById("div3").innerHTML += localStorage.getItem("username");
+//    document.getElementById("div3").innerHTML += localStorage.getItem("password");
+//  } else {
+//    document.getElementById("div3").innerHTML = "browser does not support storage";
+//  }
+//}
